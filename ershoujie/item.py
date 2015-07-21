895937f952bd10fb933fd0456eed45ccd72f3dec
yiyusheng <yiyusheng.hust@gmail.com>
2015-07-21 21:05:59 +0800

    0720: 添加所有简要信息 & 处理时间

--- ershoujie/item.py
+++ ershoujie/item.py
@@ -1,7 +1,16 @@
 import scrapy
 
 class goodItem(scrapy.Item):
+#good info
     title = scrapy.Field()
     price = scrapy.Field()
     url = scrapy.Field()
     desc = scrapy.Field()
+    time = scrapy.Field()
+    image_url =  scrapy.Field()
+    comment_count =  scrapy.Field()
+    favorite_count =  scrapy.Field()
+    
+#seller info
+    wangwang =  scrapy.Field()
+    city =  scrapy.Field()
