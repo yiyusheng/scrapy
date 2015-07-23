ea6c969b5ecd33c9e26bc5cfc83c695f3b634ecd
Books Yi <yiyusheng.hust@gmail.com>
2015-07-23 18:57:57 +0800

    0723: 只看联想

--- R_analaysis/analysis.R
+++ R_analaysis/analysis.R
@@ -3,7 +3,7 @@ require('rjson')
 dir_code <- 'D:/Git/scrapy/R_analaysis'
 dir_data <- 'D:/Data/scrapy'
 
-data <- fromJSON(file = file.path(dir_data,'0720.json'))
+data <- fromJSON(file = file.path(dir_data,'0722lenovo.json'))
 data <- data.frame(matrix(unlist(data),nrow = length(data),byrow = T))
 names(data) <- c('city','id','title','url','price','commit_count',
                  'img_url','time','favorite_count','desc')
@@ -13,7 +13,8 @@ row.names(data)
 data <- data[,col_order]
 
 data$price <- as.numeric(levels(data$price)[data$price])
+data$commit_count <- as.numeric(levels(data$commit_count)[data$commit_count])
 
-data.tp <- subset(data,(grepl('think',title) | grepl('think','desc')) & price > 500)
-data.tp <- data.tp[order(data.tp$price),]
-write.table(data.tp,file = file.path(dir_data,'0720.csv'),sep=',',row.names = F)
+data <- subset(data)
+data <- data[order(data$commit_count,decreasing = T),]
+write.table(data,file = file.path(dir_data,'0722lenovo.csv'),sep=',',row.names = F)
