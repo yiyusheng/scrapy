7ba6dec1f50c3a466a2adf25433d01405076c70e
yorkyi <yiyusheng.hust@gmail.com>
2017-07-28 21:19:52 +0800

    UPDATE: remove duplicated warning

--- secondHand/secondHand/pipelines.py
+++ secondHand/secondHand/pipelines.py
@@ -6,8 +6,7 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-import pymysql
-import datetime
+import pymysql,warnings,datetime
 
 #%% save in MySQL
 def dbHandle():
@@ -21,22 +20,33 @@ def dbHandle():
     return conn
     
 class SecondhandPipeline(object):
+    numDone = 0
+    numDup = 0
+    numError = 0
+    
     def process_item(self,item,spider):
+        warnings.filterwarnings('error', category=pymysql.Warning)
         dbObject = dbHandle()
         cursor = dbObject.cursor()
         cursor.execute("USE scrapy")
         cursor.execute("set names 'utf8';")
         cursor.execute("set character set utf8;")
         sql = "INSERT IGNORE INTO secondHand(title,uname,time,reply_count,create_time,webname,url,ext1,ext2,ext3,ext4,ext5) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-        
         try:
             cursor.execute(sql,(item['title'],item['uname'],item['time'],item['reply_count'],item['create_time'],spider.name,item['url'],
                                 item['view_count'],item['price'],item['location'],item['ext4'],item['ext5']))
             cursor.connection.commit()
-        except pymysql.IntegrityError:
-            pass
+            self.numDone += 1
         except BaseException as e:
-            print("["+datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")+"]",e,item['title'],item['uname'],item['time'],item['reply_count'],item['create_time'],spider.name,item['url'],
-                                item['view_count'],item['price'],item['location'],item['ext4'],item['ext5'])
+            if u'Duplicate entry' in str(e):
+                self.numDup += 1
+            else:
+                print("["+datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")+"]",e)
+                self.numError += 1
+#            print("["+datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")+"]",e,item['title'],item['uname'],item['time'],item['reply_count'],item['create_time'],spider.name,item['url'],
+#                                item['view_count'],item['price'],item['location'],item['ext4'],item['ext5'])
             dbObject.rollback()
         return item
+    def get_statistic(self,spider):
+        return {'numDone':self.numDone,'numDup':self.numDup,
+                'numError':self.numError,'spider':spider.name}
