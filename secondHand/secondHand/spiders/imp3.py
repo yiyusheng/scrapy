95c4df1af4e99f8a818174f95228e73b875654fb
yorkyi <yiyusheng.hust@gmail.com>
2017-12-27 16:19:04 +0800

    1227:modify imp3

--- secondHand/secondHand/spiders/imp3.py
+++ secondHand/secondHand/spiders/imp3.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from datetime import datetime
+from datetime import datetime,timedelta
 from scrapy.spiders import CrawlSpider
 from secondHand.items import SecondhandItem
 
@@ -17,10 +17,10 @@ class imp3Spider(CrawlSpider):
         rx = response.xpath("//tbody[contains(@id,'normalthread')]")
         utcTime = datetime.utcnow().replace(second=0,microsecond=0)
         for it in rx:
-           item = SecondhandItem()
+           item = SecondhandItem()         
            item['title'] = it.xpath('tr/th/a[3]/text()').extract()
            item['uname'] = it.xpath('tr/td[2]/cite/a/text()').extract()
-           item['time'] = it.xpath('tr/td[2]/em/span/text()').extract()[0].replace('.','-')+':00'
+           item['time'] = utcTime + timedelta(hours=8)
            item['reply_count'] = it.xpath('tr/td[3]/a/text()').extract()
            item['create_time'] = utcTime
            item['webname'] = self.name
